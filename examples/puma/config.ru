8a0503768e85af8ce22606df890387a47278e7f2
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-08-17 00:38:38 +1200

    Add some examples.

---
+++ examples/puma/config.ru
@@ -0,0 +1,15 @@
+require 'protocol/http/middleware'
+require_relative '../../lib/protocol/rack'
+
+# Your native application:
+middleware = Protocol::HTTP::Middleware::HelloWorld
+
+run proc{|env|
+  # Convert the rack request to a compatible rich request object:
+  request = Protocol::Rack::Request[env]
+  
+  # Call your application
+  response = middleware.call(request)
+  
+  Protocol::Rack::Adapter.make_response(env, response)
+}
---
+++ examples/server/server.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'async'
+require 'async/http/server'
+require 'async/http/client'
+require 'async/http/endpoint'
+require_relative '../../lib/protocol/rack/adapter'
+
+app = proc{|env| [200, {}, ["Hello World"]]}
+middleware = Protocol::Rack::Adapter.new(app)
+
+Async do
+  endpoint = Async::HTTP::Endpoint.parse("http://localhost:9292")
+  
+  server_task = Async(transient: true) do
+    server = Async::HTTP::Server.new(middleware, endpoint)
+    server.run
+  end
+  
+  client = Async::HTTP::Client.new(endpoint)
+  pp client.get("/").read
+end
