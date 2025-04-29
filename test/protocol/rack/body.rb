33589e6ee70752807e76f87b1e978004f7d97be6
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-04-29 15:49:00 +0900

    Ignore/close body for no entity body status codes.

---
+++ test/protocol/rack/body.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2024, by Samuel Williams.
+
+require "protocol/rack/body"
+require "protocol/http/body/readable"
+require "console"
+
+describe Protocol::Rack::Body do
+	with "#no_content?" do
+		it "returns true for status codes that indicate no content" do
+			expect(subject.no_content?(204)).to be == true
+			expect(subject.no_content?(205)).to be == true
+			expect(subject.no_content?(304)).to be == true
+			expect(subject.no_content?(200)).to be == false
+		end
+	end
+	
+	with "#wrap" do
+		let(:env) { {} }
+		let(:headers) { {} }
+		
+		it "handles nil body" do
+			expect(Console).to receive(:warn).and_return(nil)
+			
+			result = subject.wrap(env, 200, headers, nil)
+			expect(result).to be_nil
+		end
+		
+		with "non-empty body and no-content status" do
+			let(:mock_body) do
+				Protocol::HTTP::Body::Buffered.new(["content"])
+			end
+			
+			[204, 205, 304].each do |status|
+				it "closes body and returns nil for status #{status}", unique: status do
+					expect(Console).to receive(:warn).and_return(nil)
+					expect(mock_body).to receive(:close)
+					
+					result = subject.wrap(env, status, headers, mock_body)
+					
+					expect(result).to be_nil
+				end
+			end
+		end
+		
+		with "empty body and no-content status" do
+			let(:mock_body) do
+				Protocol::HTTP::Body::Buffered.new
+			end
+			
+			it "closes body and returns nil for no-content status" do
+				expect(Console).not.to receive(:warn)
+				expect(mock_body).to receive(:close)
+				
+				result = subject.wrap(env, 204, headers, mock_body)
+				
+				expect(result).to be_nil
+			end
+		end
+		
+		with "body and normal status" do
+			let(:mock_body) do
+				body = Object.new
+				
+				def body.each
+					yield "content"
+				end
+				
+				body
+			end
+			
+			it "wraps body properly with status 200" do
+				result = subject.wrap(env, 200, headers, mock_body)
+				expect(result).to be_a(Protocol::Rack::Body::Enumerable)
+				expect(result).not.to be_nil
+			end
+		end
+	end
+end
