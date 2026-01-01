90f884c2685b5068df4006961eb077348ab997ac
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2026-01-01 17:51:53 +1300

    Ensure that `HTTP_HOST` is derived from the authority.

--- test/protocol/rack/request.rb
+++ test/protocol/rack/request.rb
@@ -59,4 +59,16 @@ describe Protocol::Rack::Request do
 			)
 		end
 	end
+	
+	with "incoming request with both host header and authority" do
+		let(:headers) {Protocol::HTTP::Headers[{"host" => "header.example.com"}]}
+		let(:request) {Protocol::HTTP::Request.new("https", "authority.example.com", "GET", "/", "HTTP/1.1", headers, body)}
+		
+		it "correctly sets HTTP_HOST to the authority instead of host header" do
+			# According to HTTP/2 semantics, :authority should take precedence over the host header when both are present:
+			expect(env).to have_keys(
+				"HTTP_HOST" => be == "authority.example.com"
+			)
+		end
+	end
 end
