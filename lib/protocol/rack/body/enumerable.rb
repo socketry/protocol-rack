1c20444c978a4fd40ea761b2c7ff1d23fe2d46db
Samuel Williams <samuel.williams@shopify.com>
2026-09-10 19:04:32 +1200

    Simplify closed body enumeration
    Assisted-By: devx/b8194217-b20a-4799-b266-4910beb3ee2b

--- lib/protocol/rack/body/enumerable.rb
+++ lib/protocol/rack/body/enumerable.rb
@@ -103,9 +103,7 @@ module Protocol
 				# @yields {|chunk| ...}
 				# 	@parameter chunk [String] A chunk of the response body.
 				def each(&block)
-					if body = @body
-						body.each(&block)
-					end
+					@body&.each(&block)
 				rescue => error
 					raise
 				ensure
